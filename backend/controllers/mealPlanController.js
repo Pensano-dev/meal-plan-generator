58b0b5c5a5d3af2432f0ab5a0d88ef413abf299c
Sidra Iqbal <sidra1360@gmail.com>
2023-08-01 11:44:17 +0100

    edited controller with a try catch block

--- backend/controllers/mealPlanController.js
+++ backend/controllers/mealPlanController.js
@@ -1,16 +1,26 @@
 const openai = require('../config/openaiConfig');
 
 const generateMealPlan = async (mealInputs) => {
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: 'user',
+          content: `Come up with a meal plan for the inputs ${mealInputs}`
+        }
+      ]
+    });
 
-  const mealPlan = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: 'user',
-        content: `Come up with a mealplan for the inputs ${mealInputs}`
-      }
-    ]
-  })
+    // Extract the chat response from the API response
+    const chatResponse = response.data?.choices?.[0]?.message?.content || '';
+    
+    return chatResponse;
+  } catch (error) {
+    console.error('OpenAI API Error:', error);
+
+    throw new Error('Failed to generate meal plan. Please try again later.');
+  }
 }
 
 module.exports = generateMealPlan;
