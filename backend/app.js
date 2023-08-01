24f6d7d7531cc2620e6829ae944c0d8624243848
Sidra Iqbal <sidra1360@gmail.com>
2023-08-01 22:11:12 +0100

    CORS added for different hosts on FE and BE

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mealPlanRoutes = require('./routes/mealPlanRoutes');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use('/api', mealPlanRoutes);
