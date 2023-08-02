03e1a559cf205288ed00a43bc6888ff3607e550b
natashabuckham <natashaisobel@gmail.com>
2023-08-02 15:01:25 +0100

    Add CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,21 @@ const mealPlanRoutes = require('./routes/mealPlanRoutes');
 
 app.use(bodyParser.json());
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000', // Development frontend URL - this can also be set as a ENV variable and imported
+  // add production frontend URL here once deployed
+]
+
+// CORS configuration so frontend can only access backend from allowed origins
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+}));
 
 // Routes
 app.use('/api', mealPlanRoutes);
