043ac4c0aa7bb0a193ee8faf8627a9dcf45dd81d
Keremet <keremet@Keremets-MacBook-Air.local>
2023-08-10 02:44:06 +0100

    completed submit button

--- frontend/src/components/meal-plan-form/meal-plan-form.js
+++ frontend/src/components/meal-plan-form/meal-plan-form.js
@@ -1,5 +1,7 @@
 import * as React from 'react'
-import { ChakraProvider, Container, Heading, Button, Box, Divider, Text, FormControl, FormLabel, RadioGroup, Stack, Radio } from '@chakra-ui/react'
+import { ChakraProvider, Container, Heading, Button, Box, Divider, Text, FormControl, FormLabel, RadioGroup, Stack, Radio, Checkbox, Flex, Input, Center } from '@chakra-ui/react'
+// import { PiEgg } from 'react-icons/fa';
+
 
 function MealPlanForm({ onChange, onSubmit, isValid }) {
 
@@ -30,15 +32,15 @@ function MealPlanForm({ onChange, onSubmit, isValid }) {
       </Box>
       
       <Button bg='#ffb301' mt={8} borderRadius='3xl'>Lets Get Started!</Button>
-         <Divider mt={10} mb={-3}/> 
+        <Divider mt={10} mb={-3}/> 
       <Box as="button" size='lg' borderRadius='3xl' bg='gray.300' color='gray.600' px={4} position="relative" zIndex="2">User options</Box>
       </Container>
+
       {/* Form below */}
       <Container maxW="container.md">
-        <form>
-          <FormControl mb={4}>
-          <Heading>01</Heading> 
-            <FormLabel>What age group is your meal plan for?</FormLabel>
+          <FormControl mb={6}>
+          <Heading my={4}>01</Heading> 
+            <FormLabel mb={4}>What age group is your meal plan for?</FormLabel>
             <RadioGroup onChange={onChange}>
               <Stack spacing={4} direction='row'>
                 {/* values need to be double-checked */}
@@ -48,113 +50,50 @@ function MealPlanForm({ onChange, onSubmit, isValid }) {
               </Stack>
             </RadioGroup>
           </FormControl>
-          <Heading>02</Heading> 
-        <Text>Any food allergies or intolerances?</Text>
-        Other (please separate each food item with a comma",.")
 
-
-          {/* Other form fields go here */}
-
-          {/* Submit button */}
-        </form>
-        <Box mt={8}>
-       
-
-<Heading>03</Heading> 
-<Text>Tick the boxes which apply to you:
-</Text>
-
-Mixed Food Diet
-Paleo
-Low Carb
-Pescetarian
-Any food that you want to include
-(please separate each food item with a comma",.")
-CREATE YOUR MEAL PLAN
-User Options
-Adult
-over 12 years
-oUr
-Vegan
-Vegetarian
-      </Box>
-      
-      </Container>
-      
+          <FormControl mb={4}>
+            <Heading my={4}>02</Heading> 
+        <FormLabel mb={4}>Any food allergies or intolerances?</FormLabel>
+        <Flex spacing={6} direction='row' mb={6}>
+        <Checkbox w='150px'>Lactose</Checkbox>
+        <Checkbox w='150px'>Gluten</Checkbox>
+        <Checkbox w='150px'>Wheat</Checkbox>
+        <Checkbox w='150px'>Eggs</Checkbox>
+        <Checkbox w='150px'>Peanuts</Checkbox>
+        </Flex>
+        <Flex spacing={6} direction='row' mb={6}>
+        <Checkbox w='150px'>Tree nuts</Checkbox>
+        <Checkbox w='150px'>Soybeans</Checkbox>
+        <Checkbox w='150px'>Sesame seeds</Checkbox>
+        <Checkbox w='150px'>Shellfish</Checkbox>
+        <Checkbox w='150px'>Fish</Checkbox>
+        </Flex>
+        <Text mb={4}>Other (please separate each food item with a comma ",")</Text>
+        <Input type="text"></Input>
+        </FormControl>
+          <FormControl>
+            <Heading my={4}>03</Heading> 
+            <FormLabel mb={4}>Tick the boxes which apply to you:</FormLabel>
+            <Flex spacing={6} direction='row' mb={6}>
+              <Checkbox w='200px'>Mixed Food Diet</Checkbox>
+              <Checkbox w='200px'>Paleo</Checkbox>
+              <Checkbox w='200px'>Vegan</Checkbox>
+              </Flex>
+              <Flex spacing={6} direction='row' mb={6}>
+              <Checkbox w='200px'>Low Carb</Checkbox>
+              <Checkbox w='200px'>Pescetarian</Checkbox>
+              <Checkbox w='200px'>Vegetarian</Checkbox>  
+              </Flex>
+            <Text mb={4}>Any food that you want to include
+            (please separate each food item with a comma ",")</Text>
+            <Input type="text"></Input>  
+          </FormControl>
+          <Center my={10}>
+            <Button bg='#ffb301'  borderRadius='3xl'>CREATE YOUR MEAL PLAN</Button>
+          </Center>
+      </Container>     
     </ChakraProvider> 
   );
 }
 
-
-
 export default MealPlanForm;
-
-// inline CSS for now but may need to move this out to a separate file if we have more later
-  // const requiredStyle = {
-  //   color: 'red',
-  //   flex: 1,
-  //   marginLeft: '5px',
-  // };
-
-  // const containerStyle = {
-  //   display: 'flex',
-  // };
-
-{/* <> 
-    <p>Choose your options:</p>
-      <form>
-        <div style={containerStyle}>
-          <p>Which age group is your meal plan for?</p><p style={requiredStyle}>*</p>
-        </div>
-        <input type="radio" id="age1" name="age" value="6-24" required onChange={onChange}/>
-        <label htmlFor="age">6-24 months</label>
-        <input type="radio" id="age2" name="age" value="3-12" onChange={onChange}/>
-        <label htmlFor="age">3-12 years old</label>
-        <input type="radio" id="age3" name="age" value="12+" onChange={onChange}/>
-        <label htmlFor="age">12+ years old</label>
-
-        <p>Any food allergies or intolerances?</p>
-        <input type="checkbox" id="allergies" value="shellfish" onChange={onChange}/>
-        <label htmlFor="allergies">Crustacean shellfish (eg. crab, lobster)</label>
-        <input type="checkbox" id="allergies" value="eggs" onChange={onChange}/>
-        <label htmlFor="allergies">Eggs</label>
-        <input type="checkbox" id="allergies" value="fish" onChange={onChange}/>
-        <label htmlFor="allergies">Fish (eg. bass, flounder, cod)</label>
-        <input type="checkbox" id="allergies" value="gluten" onChange={onChange}/>
-        <label htmlFor="allergies">Gluten</label>
-        <input type="checkbox" id="allergies" value="lactose" onChange={onChange}/>
-        <label htmlFor="allergies">Lactose</label>
-        <input type="checkbox" id="allergies" value="peanuts" onChange={onChange}/>
-        <label htmlFor="allergies">Peanuts</label>
-        <input type="checkbox" id="allergies" value="sesame" onChange={onChange}/>
-        <label htmlFor="allergies">Sesame</label>
-        <input type="checkbox" id="allergies" value="soybeans" onChange={onChange}/>
-        <label htmlFor="allergies">Soybeans</label>
-        <input type="checkbox" id="allergies" value="treenuts" onChange={onChange}/>
-        <label htmlFor="allergies">Tree nuts (eg. almonds, walnuts, peacans)</label>
-        <input type="checkbox" id="allergies" value="wheat" onChange={onChange}/>
-        <label htmlFor="allergies">Wheat</label>
-        <p>Please input any other intolerances</p>
-        <input type="text" id="other-food-intolerances" name="other-food-intolerance" placeholder="Enter foods here separated by commas" onChange={onChange} style={{ border: isValid ? '' : '1px solid red' }} />
-
-        <p>Please tick which boxes relate to your current diet</p>
-        <input type="checkbox" id="diet" value="mixed" onChange={onChange}/>
-        <label htmlFor="diet">Mixed food diet (animal and vegetable sources)</label>
-        <input type="checkbox" id="diet" value="pescatarian" onChange={onChange}/>
-        <label htmlFor="diet">Pescatarian</label>
-        <input type="checkbox" id="diet" value="starch" onChange={onChange}/>
-        <label htmlFor="diet">Starch/Carbohydrate rescriction (low carb)</label>
-        <input type="checkbox" id="diet" value="vegan" onChange={onChange}/>
-        <label htmlFor="diet">Vegan</label>
-        <input type="checkbox" id="diet" value="vegetarian" onChange={onChange}/>
-        <label htmlFor="diet">Vegetarian</label>
-        <input type="checkbox" id="diet" value="paleo" onChange={onChange}/>
-        <label htmlFor="diet">Paleo</label>
-
-        <p>Are there any foods which you want the mealplan to include? (separated by comma ",")</p>
-        <input type="text" id="other-food-include" name="other-food-include" placeholder="Enter foods here separated by commas" onChange={onChange} style={{ border: isValid ? '' : '1px solid red' }} />
-
-        <p>Please submit when ready</p>
-        <button type="submit" onClick={onSubmit}>Submit</button>
-      </form>
-    </>  */}
