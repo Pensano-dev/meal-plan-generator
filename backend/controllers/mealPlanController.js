b7c1022323e20cf176cea66d476e712211021e30
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-08-24 17:32:14 +0100

    Add a role and content to the controller

--- backend/controllers/mealPlanController.js
+++ backend/controllers/mealPlanController.js
@@ -1,58 +1,64 @@
-const openai = require('../config/openaiConfig');
+const openai = require("../config/openaiConfig");
 
 const mealPlanController = {
-//   generateMealPlan: (req, res) => {
-//     try {
-//       const mealInputs = req.body;
-//       console.log('Here are your meal inputs:', mealInputs);
-//       res.status(200).json({ message: 'Meal plan generated successfully.' });
-//       // Your meal plan generation code using OpenAI can go here
-//       // For now, we're just logging the meal inputs
+  //   generateMealPlan: (req, res) => {
+  //     try {
+  //       const mealInputs = req.body;
+  //       console.log('Here are your meal inputs:', mealInputs);
+  //       res.status(200).json({ message: 'Meal plan generated successfully.' });
+  //       // Your meal plan generation code using OpenAI can go here
+  //       // For now, we're just logging the meal inputs
 
-//     } catch (error) {
-//       console.error('Error:', error);
-//       res.status(500).json({ error: 'Failed to generate meal plan. Please try again later.' });
-//     }
-//   }
-// };
+  //     } catch (error) {
+  //       console.error('Error:', error);
+  //       res.status(500).json({ error: 'Failed to generate meal plan. Please try again later.' });
+  //     }
+  //   }
+  // };
 
-// const generateMealPlan = async (mealInputs) => {
+  // const generateMealPlan = async (mealInputs) => {
   generateMealPlan: async (req, res) => {
-  try {
-
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: 'user',
-          content: 'Tell me a joke, please.'
-          // content: `Come up with a meal plan for the inputs ${mealInputs}`
-        }
-      ]
-    });
-    
-    console.log(response.data.choices[0].message.content);
+    try {
+      const mealInputs = req.body;
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `I am a nutritional therapist. You are an assistant that helps me to generate 7-day meal 
+            plans for my clients. I want the meal plan to be in a table, with the days of the week across the 
+            top and the meals on the left-hand column. The meals should include breakfast, lunch, dinner and 
+            one snack per day. I would like each meal suggestion to include the three food groups of protein, carbohydrates 
+            and healthy fats. Please adapt the meal plan according to the client's characteristics and 
+            preferences.`,
+          },
+        ],
+      });
+      // 7-day plan, table format, with the days on top, with an optional snack,and each meal should have the three food groups (protein, carbohydrates and healthy fats)
+      console.log('Here are your meal inputs:', mealInputs);
+      console.log(response.data.choices[0].message.content);
 
       // Send a response back to the client
-      res.status(200).json({ message: 'Meal plan generated successfully' });
+      res.status(200).json({ message: "Meal plan generated successfully" });
     } catch (error) {
-      console.error('Error generating meal plan:', error);
-      res.status(500).json({ error: 'An error occurred while generating the meal plan' });
+      console.error("Error generating meal plan:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while generating the meal plan" });
     }
-  }
+  },
 };
 
-    
-  //   // Extract the chat response from the API response
-  //   const chatResponse = response.data?.choices?.[0]?.message?.content || '';
+//   // Extract the chat response from the API response
+//   const chatResponse = response.data?.choices?.[0]?.message?.content || '';
 
-  //   console.log(chatResponse);
-  //   // return chatResponse;
-  // } catch (error) {
-  //   console.error('OpenAI API Error:', error);
+//   console.log(chatResponse);
+//   // return chatResponse;
+// } catch (error) {
+//   console.error('OpenAI API Error:', error);
 
-  //   throw new Error('Failed to generate meal plan. Please try again later.');
-  //   }
-  // }
+//   throw new Error('Failed to generate meal plan. Please try again later.');
+//   }
+// }
 
 module.exports = mealPlanController;
