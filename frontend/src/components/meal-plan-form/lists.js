cd0bbdf125e97900384e2a74e55f90f4c9eb5a90
Pablo <pablito@me.com>
2023-09-20 11:25:53 +0100

    Added code to see allergy icons in original colours or uniform colours in lists.js

--- frontend/src/components/meal-plan-form/lists.js
+++ frontend/src/components/meal-plan-form/lists.js
@@ -3,20 +3,33 @@ import { EggIcon, FishIcon, MilkIcon, PeanutIcon, SoyaIcon, MolluscIcon, NutsIco
 export const foodAllergies = ["Eggs", "Fish", "Gluten", "Milk", "Peanuts", "Shellfish", "Soy", "Tree nuts"];
 
 const size = 100;
-const iconColor = "rgb(157, 183, 175)";
+const iconColour = "rgb(157, 183, 175)";
 const radius = "50px";
 const border = "1px solid rgb(74, 84, 81)"
 
+// UNIFORM ICON COLOURS specified in iconColour above
 export const foodAllergyIcons = [
-  <EggIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <FishIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <GlutenIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <MilkIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <PeanutIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <MolluscIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <SoyaIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />,
-  <NutsIcon width={size} height={size} outerColor={iconColor} wrapperStyle={{ borderRadius: radius, border: border}} />
+  <EggIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <FishIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <GlutenIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <MilkIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <PeanutIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <MolluscIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <SoyaIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />,
+  <NutsIcon width={size} height={size} outerColor={iconColour} wrapperStyle={{ borderRadius: radius, border: border}} />
 ];
+
+// // ORIGINAL ICON COLOURS
+// export const foodAllergyIcons = [
+//   <EggIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <FishIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <GlutenIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <MilkIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <PeanutIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <MolluscIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <SoyaIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />,
+//   <NutsIcon width={size} height={size} wrapperStyle={{ borderRadius: radius, border: border}} />
+// ];
   
 export const ageGroups = ["6-24", "3-12", "12+"];
 
