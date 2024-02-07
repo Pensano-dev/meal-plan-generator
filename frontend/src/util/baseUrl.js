4a2cdeae7e7285b72dc5a52ca255cd2fc5e33cf3
Pablo <pablito@me.com>
2024-02-07 20:07:30 +0000

    Added baseUrl file to provide conditional base url for the backend

---
+++ frontend/src/util/baseUrl.js
@@ -0,0 +1,4 @@
+export default process.env.NODE_ENV === 'deploy'
+  ? 'https://almost-meal-planner-backend.onrender.com'
+  : 'https://almost-meal-planner-backend.onrender.com'
+  // : 'http://localhost:8000';
